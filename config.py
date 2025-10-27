ebb42c809955521a859cd17beca6903376e28648
amary-ann <marynaj28@gmail.com>
2025-10-27 19:54:45 +0100

    changes

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-openai_api_key = os.getenv("OPENAI_API_KEY", "your-openai-key")  # replace with your key
+# openai_api_key = os.getenv("OPENAI_API_KEY", "your-openai-key")  # replace with your key
 subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "your-azure-key") # replace with your key
 endpoint = os.getenv("AZURE_OPENAI_ENDPOINT", "https://your-resource-name.openai.azure.com/") # replace with your endpoint
 deployment = os.getenv("DEPLOYMENT_NAME", "deployment-name") # replace with your deployment name
 embed_deployment = os.getenv("EMBED_DEPLOYMENT")
 
 DB_CONFIG = {
-    "host": "pennyfy-staging.cpuqassmwrgm.us-east-1.rds.amazonaws.com",
-    "user": "pennyfy",
-    "password": "Freda29%",
-    "database": "pennyfy",
-    "port": 5432,
+    "host": os.getenv("PG_HOST"),
+    "user": os.getenv("PG_USER"),
+    "password": os.getenv("PG_PWD"),
+    "database": os.getenv("PG_DB"),
+    "port": os.getenv("PORT"),
 }
